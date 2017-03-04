60293d403cbd1dda63fdec1eba6f2ab4a269bb83
dlouiset <dlouiset@MacBook-Pro-de-David-LOUISET.local>
2017-03-04 23:03:06 +0100

    framework symfony 2.8

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# DlsCab
+<!--
+@Author: David LOUISET <dlouiset>
+@Date:   2017-03-04T19:18:49+01:00
+@Last modified by:   dlouiset
+@Last modified time: 2017-03-04T22:53:22+01:00
+-->
+
+
+
+DlsCab Standard Edition
+=======================
+
 Simple Manager For Clients Missions Documents (Symfony)
---
+++ web/app_dev.php
@@ -0,0 +1,37 @@
+<?php
+# @Author: David LOUISET <dlouiset>
+# @Date:   2017-02-06T14:09:50+01:00
+# @Last modified by:   dlouiset
+# @Last modified time: 2017-03-02T16:08:16+01:00
+
+
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
+// for more information
+//umask(0000);
+
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
+//if (isset($_SERVER['HTTP_CLIENT_IP'])
+//    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+//    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')) || php_sapi_name() === 'cli-server')
+//) {
+//    header('HTTP/1.0 403 Forbidden');
+//    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+//}
+
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require __DIR__.'/../app/autoload.php';
+Debug::enable();
+
+$kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
